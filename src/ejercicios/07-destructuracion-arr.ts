ba34259217d81bd7d8d028c77672d5bf86d4d607
HDS <jonathan.canales.hds@gmail.com>
2021-07-23 16:28:52 -0500

    Destructuración de arreglos

---
+++ src/ejercicios/07-destructuracion-arr.ts
@@ -0,0 +1,10 @@
+/*
+    ===== Destructuración de arreglos =====
+*/
+
+const dbz: string[] = ['Goku','Vegeta','Gohan','Trunks']
+const [ , ,p3 ] = dbz
+
+// console.log('Personaje 1: ', p1);
+// console.log('Personaje 2: ', p2);
+console.log('Personaje 3: ', p3);
