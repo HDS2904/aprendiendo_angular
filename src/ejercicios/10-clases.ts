546895fa096954a086acf8203e42317115f24318
HDS <jonathan.canales.hds@gmail.com>
2021-07-24 19:11:11 -0500

    Extender una clase

--- src/ejercicios/10-clases.ts
+++ src/ejercicios/10-clases.ts
@@ -2,15 +2,23 @@
     ===== Código de TypeScript =====
 */
 
-class Heroe {
-  // alterEgo: string;
-  // age: number;
-  // fistName: string;
+class Person {
+  constructor(
+    public name: string,
+    public address: string,
+  ) {}
+}
+class Heroe extends Person{
   constructor( 
     public alterEgo: string,
     public age: number,
     public firstName: string
-  ) {}
+  ) {
+    super(
+      firstName,
+      'New York, EE.UU',
+    )
+  }
 }
 
 const ironman = new Heroe('Iroman',40,'Tony');
