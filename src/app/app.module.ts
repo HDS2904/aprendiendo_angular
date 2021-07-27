4002d3e8935e20c92c441948ab387d85d1486dc4
HDS <jonathan.canales.hds@gmail.com>
2021-07-26 23:07:25 -0500

    Módulos - segunda parte

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ContadorModule } from './contador/contador.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ContadorComponent } from './contador/contador.component';
-
-import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContadorComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HeroesModule,
+    ContadorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
