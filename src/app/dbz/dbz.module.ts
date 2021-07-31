6c5e2c8008d17579a25dc19f329329898ec4308b
SteveArias <steve.arias@devmente.com>
2021-07-31 02:49:24 -0500

    Crear componentes hijos

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 
 import { FormsModule } from '@angular/forms';
+import { CharactersComponent } from './characters/characters.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    CharactersComponent
   ],
   exports:[
     HomeComponent
