5ea65780acefe67edc3eac888bf92b6e237abeb7
HDS <jonathan.canales.hds@gmail.com>
2021-08-13 22:09:07 -0500

    Realizando peticiones http

--- src/app/gifs/service/gifs.service.ts
+++ src/app/gifs/service/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,9 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
 	
+	private API_KEY = 'BqoCcLGdiTS5It8Qdu6pQDMdplWyBvHH';
+	private URL = 'api.giphy.com/v1/gifs/search';
 	private _historial: string[] = [];
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
 
   get historial() {
 	return [...this._historial]
@@ -19,5 +22,9 @@ export class GifsService {
 			this._historial.unshift(query);
 			this._historial = this._historial.splice(0,10)
 		}
+		this.http.get('http://api.giphy.com/v1/gifs/search?api_key=BqoCcLGdiTS5It8Qdu6pQDMdplWyBvHH&q=dragon ball z&limit=10')
+			.subscribe( (resp: any) => {
+				console.log(resp.data);
+			})
   }
 }
