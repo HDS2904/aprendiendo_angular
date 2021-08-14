f46724c1d3201d441823daa6787ff6445a7725e0
HDS <jonathan.canales.hds@gmail.com>
2021-08-13 19:49:43 -0500

    @viewChild - Obteniendo referencias a objetos HTML

--- src/app/gifs/searches/searches.component.ts
+++ src/app/gifs/searches/searches.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-searches',
@@ -7,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class SearchesComponent implements OnInit {
+	// "!": Permite asegurar que la variable nunca será indefinida
+	@ViewChild("inputText") inputText!: ElementRef<HTMLInputElement>
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  search(){
+	console.log("Valor: ", this.inputText.nativeElement.value );
+	this.inputText.nativeElement.value = ''
   }
 
 }
